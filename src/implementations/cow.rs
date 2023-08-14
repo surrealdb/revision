10f30f8f2a0379547e371a563e992ff64f1e2d09
Mees Delzenne <mees.delzenne@gmail.com>
2023-08-14 11:36:22 +0200

    Add tests

--- src/implementations/cow.rs
+++ src/implementations/cow.rs
@@ -4,7 +4,7 @@ use crate::Revisioned;
 
 impl<T> Revisioned for Cow<'_, T>
 where
-	T: ?Sized + ToOwned + Revisioned,
+	T: Sized + ToOwned + Revisioned,
 	T::Owned: Revisioned,
 {
 	fn revision() -> u16 {
@@ -15,10 +15,39 @@ where
 		(**self).serialize_revisioned(w)
 	}
 
-	fn deserialize_revisioned<R: std::io::Read>(r: &mut R) -> Result<Self, crate::Error>
-	where
-		Self: Sized,
-	{
+	fn deserialize_revisioned<R: std::io::Read>(r: &mut R) -> Result<Self, crate::Error> {
 		T::Owned::deserialize_revisioned(r).map(Cow::Owned)
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::Revisioned;
+	use std::borrow::Cow;
+
+	#[test]
+	fn cow_borrow() {
+		let number = 20u8;
+
+		let cow = Cow::Borrowed(&number);
+		let mut mem = Vec::new();
+		cow.serialize_revisioned(&mut mem).unwrap();
+		assert_eq!(mem.len(), 1);
+		let out = Cow::<u8>::deserialize_revisioned(&mut mem.as_slice()).unwrap();
+		assert!(matches!(out, Cow::Owned(_)));
+		assert_eq!(*out, number)
+	}
+
+	#[test]
+	fn cow_owned() {
+		let number = 20u8;
+
+		let cow: Cow<u8> = Cow::Owned(number);
+		let mut mem = Vec::new();
+		cow.serialize_revisioned(&mut mem).unwrap();
+		assert_eq!(mem.len(), 1);
+		let out = Cow::<u8>::deserialize_revisioned(&mut mem.as_slice()).unwrap();
+		assert!(matches!(out, Cow::Owned(_)));
+		assert_eq!(*out, number)
 	}
 }
