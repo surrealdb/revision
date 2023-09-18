1be873b31c1dcd32c711cc48ea4e8d7784e4832e
DreamingCodes <me@dreaming.codes>
2023-09-18 11:04:39 +0200

    Add chrono NaiveDate support

--- src/implementations/chrono.rs
+++ src/implementations/chrono.rs
@@ -1,6 +1,6 @@
 use super::super::Error;
 use super::super::Revisioned;
-use chrono::{offset::TimeZone, DateTime, Utc};
+use chrono::{offset::TimeZone, DateTime, Utc, NaiveDate, Datelike};
 
 impl Revisioned for DateTime<Utc> {
 	#[inline]
@@ -24,9 +24,32 @@ impl Revisioned for DateTime<Utc> {
 	}
 }
 
+impl Revisioned for NaiveDate {
+	#[inline]
+	fn serialize_revisioned<W: std::io::Write>(&self, writer: &mut W) -> Result<(), Error> {
+		self.year().serialize_revisioned(writer)?;
+		self.month().serialize_revisioned(writer)?;
+		self.day().serialize_revisioned(writer)?;
+		Ok(())
+	}
+
+	#[inline]
+	fn deserialize_revisioned<R: std::io::Read>(reader: &mut R) -> Result<Self, Error> {
+		let year = <i32 as Revisioned>::deserialize_revisioned(reader)?;
+		let month = <u32 as Revisioned>::deserialize_revisioned(reader)?;
+		let day = <u32 as Revisioned>::deserialize_revisioned(reader)?;
+		Ok(NaiveDate::from_ymd_opt(year, month, day)
+			.ok_or_else(|| Error::Deserialize("invalid date".to_string()))?)
+	}
+
+	fn revision() -> u16 {
+		1
+	}
+}
+
 #[cfg(test)]
 mod tests {
-
+	use chrono::NaiveDate;
 	use super::DateTime;
 	use super::Revisioned;
 	use super::Utc;
@@ -50,6 +73,28 @@ mod tests {
 		assert_eq!(mem.len(), 14);
 		let out =
 			<DateTime<Utc> as Revisioned>::deserialize_revisioned(&mut mem.as_slice()).unwrap();
+		assert_eq!(val, out);
+	}
+
+	#[test]
+	fn test_naive_date_min() {
+		let val = NaiveDate::from_ymd_opt(1, 1, 1).unwrap();
+		let mut mem: Vec<u8> = vec![];
+		val.serialize_revisioned(&mut mem).unwrap();
+		assert_eq!(mem.len(), 3);
+		let out =
+			<NaiveDate as Revisioned>::deserialize_revisioned(&mut mem.as_slice()).unwrap();
+		assert_eq!(val, out);
+	}
+
+	#[test]
+	fn test_naive_date_max() {
+		let val = NaiveDate::from_ymd_opt(9999, 12, 31).unwrap();
+		let mut mem: Vec<u8> = vec![];
+		val.serialize_revisioned(&mut mem).unwrap();
+		assert_eq!(mem.len(), 5);
+		let out =
+			<NaiveDate as Revisioned>::deserialize_revisioned(&mut mem.as_slice()).unwrap();
 		assert_eq!(val, out);
 	}
 }
