0c8de4646940e42b51dbea3df0796ff5f63b188c
DreamingCodes <me@dreaming.codes>
2023-09-18 12:15:34 +0200

    Add chrono NaiveTime support

--- src/implementations/chrono.rs
+++ src/implementations/chrono.rs
@@ -1,6 +1,6 @@
 use super::super::Error;
 use super::super::Revisioned;
-use chrono::{offset::TimeZone, DateTime, Utc, NaiveDate, Datelike};
+use chrono::{offset::TimeZone, DateTime, Utc, NaiveDate, Datelike, NaiveTime, Timelike};
 
 impl Revisioned for DateTime<Utc> {
 	#[inline]
@@ -38,8 +38,33 @@ impl Revisioned for NaiveDate {
 		let year = <i32 as Revisioned>::deserialize_revisioned(reader)?;
 		let month = <u32 as Revisioned>::deserialize_revisioned(reader)?;
 		let day = <u32 as Revisioned>::deserialize_revisioned(reader)?;
-		Ok(NaiveDate::from_ymd_opt(year, month, day)
-			.ok_or_else(|| Error::Deserialize("invalid date".to_string()))?)
+		NaiveDate::from_ymd_opt(year, month, day)
+			.ok_or_else(|| Error::Deserialize("invalid date".to_string()))
+	}
+
+	fn revision() -> u16 {
+		1
+	}
+}
+
+impl Revisioned for NaiveTime {
+	#[inline]
+	fn serialize_revisioned<W: std::io::Write>(&self, writer: &mut W) -> Result<(), Error> {
+		self.hour().serialize_revisioned(writer)?;
+		self.minute().serialize_revisioned(writer)?;
+		self.second().serialize_revisioned(writer)?;
+		self.nanosecond().serialize_revisioned(writer)?;
+		Ok(())
+	}
+
+	#[inline]
+	fn deserialize_revisioned<R: std::io::Read>(reader: &mut R) -> Result<Self, Error> {
+		let hour = <u32 as Revisioned>::deserialize_revisioned(reader)?;
+		let minute = <u32 as Revisioned>::deserialize_revisioned(reader)?;
+		let second = <u32 as Revisioned>::deserialize_revisioned(reader)?;
+		let nano = <u32 as Revisioned>::deserialize_revisioned(reader)?;
+		NaiveTime::from_hms_nano_opt(hour, minute, second, nano)
+			.ok_or_else(|| Error::Deserialize("invalid time".to_string()))
 	}
 
 	fn revision() -> u16 {
@@ -49,7 +74,7 @@ impl Revisioned for NaiveDate {
 
 #[cfg(test)]
 mod tests {
-	use chrono::NaiveDate;
+	use chrono::{NaiveDate, NaiveTime};
 	use super::DateTime;
 	use super::Revisioned;
 	use super::Utc;
@@ -95,6 +120,28 @@ mod tests {
 		assert_eq!(mem.len(), 5);
 		let out =
 			<NaiveDate as Revisioned>::deserialize_revisioned(&mut mem.as_slice()).unwrap();
+		assert_eq!(val, out);
+	}
+
+	#[test]
+	fn test_naive_time_min() {
+		let val = NaiveTime::from_hms_nano_opt(0, 0, 0, 0).unwrap();
+		let mut mem: Vec<u8> = vec![];
+		val.serialize_revisioned(&mut mem).unwrap();
+		assert_eq!(mem.len(), 4);
+		let out =
+			<NaiveTime as Revisioned>::deserialize_revisioned(&mut mem.as_slice()).unwrap();
+		assert_eq!(val, out);
+	}
+
+	#[test]
+	fn test_naive_time_max() {
+		let val = NaiveTime::from_hms_nano_opt(23, 59, 59, 999_999_999).unwrap();
+		let mut mem: Vec<u8> = vec![];
+		val.serialize_revisioned(&mut mem).unwrap();
+		assert_eq!(mem.len(), 8);
+		let out =
+			<NaiveTime as Revisioned>::deserialize_revisioned(&mut mem.as_slice()).unwrap();
 		assert_eq!(val, out);
 	}
 }
