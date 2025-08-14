0e5ea8956c0814c1197259930c61ab1391005f8c
Rushmore Mushambi <rushmore@surrealdb.com>
2025-08-14 15:33:02 +0100

    impl<T> Revisioned for Arc<T> (#32)

---
+++ src/implementations/arc.rs
@@ -0,0 +1,38 @@
+use super::super::Error;
+use super::super::Revisioned;
+use std::sync::Arc;
+
+impl<T> Revisioned for Arc<T>
+where
+	T: Revisioned,
+{
+	#[inline]
+	fn serialize_revisioned<W: std::io::Write>(&self, writer: &mut W) -> Result<(), Error> {
+		self.as_ref().serialize_revisioned(writer)
+	}
+
+	#[inline]
+	fn deserialize_revisioned<R: std::io::Read>(reader: &mut R) -> Result<Self, Error> {
+		Ok(Arc::new(T::deserialize_revisioned(reader)?))
+	}
+
+	fn revision() -> u16 {
+		1
+	}
+}
+
+#[cfg(test)]
+mod tests {
+
+	use super::*;
+
+	#[test]
+	fn test_arc() {
+		let val = Arc::new(u32::MAX);
+		let mut mem: Vec<u8> = vec![];
+		val.serialize_revisioned(&mut mem).unwrap();
+		assert_eq!(mem.len(), 5);
+		let out = Revisioned::deserialize_revisioned(&mut mem.as_slice()).unwrap();
+		assert_eq!(val, out);
+	}
+}
