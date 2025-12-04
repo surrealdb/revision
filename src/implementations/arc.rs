d0bdd1b4f23e73c3f51b5f3087d8fce418bbb685
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2025-12-04 11:35:58 +0000

    Add support `Arc<str>`

--- src/implementations/arc.rs
+++ src/implementations/arc.rs
@@ -35,6 +35,28 @@ where
 	}
 }
 
+// Specialized implementations for Arc<str>
+impl SerializeRevisioned for Arc<str> {
+	#[inline]
+	fn serialize_revisioned<W: std::io::Write>(&self, writer: &mut W) -> Result<(), Error> {
+		self.as_ref().serialize_revisioned(writer)
+	}
+}
+
+impl DeserializeRevisioned for Arc<str> {
+	#[inline]
+	fn deserialize_revisioned<R: std::io::Read>(reader: &mut R) -> Result<Self, Error> {
+		String::deserialize_revisioned(reader).map(Arc::from)
+	}
+}
+
+impl Revisioned for Arc<str> {
+	#[inline]
+	fn revision() -> u16 {
+		1
+	}
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -47,6 +69,18 @@ mod tests {
 		val.serialize_revisioned(&mut mem).unwrap();
 		assert_eq!(mem.len(), 5);
 		let out = DeserializeRevisioned::deserialize_revisioned(&mut mem.as_slice()).unwrap();
+		assert_eq!(val, out);
+	}
+
+	#[test]
+	fn test_arc_str() {
+		let val: Arc<str> = Arc::from("hello world");
+		let mut mem: Vec<u8> = vec![];
+		val.serialize_revisioned(&mut mem).unwrap();
+		assert_eq!(mem.len(), 12); // 11 chars + 1 byte for length encoding
+		let out: Arc<str> =
+			<Arc<str> as DeserializeRevisioned>::deserialize_revisioned(&mut mem.as_slice())
+				.unwrap();
 		assert_eq!(val, out);
 	}
 }
